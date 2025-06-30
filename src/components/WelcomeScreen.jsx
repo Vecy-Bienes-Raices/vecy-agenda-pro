987f69c918b7de42641872f125be664650ff34b0
Vecy Bienes Raíces <vecybienesraices@gmail.com>
2025-06-30 18:11:58 -0500

    fix: Final corrections to routing and form links

--- src/components/WelcomeScreen.jsx
+++ src/components/WelcomeScreen.jsx
@@ -34,7 +34,8 @@ function WelcomeScreen({ onStart }) {
             <span className="ml-2">Por tu seguridad y la nuestra es clave ingresar datos reales para agilizar el permisos de ingreso.</span>
           </p>
           <ul className="list-disc list-inside pl-4 space-y-2">
-            <strong>🤝 ¡Gracias por confiar en nosotros!</strong>
+            <span className="mt-1">🤝</span>
+            <strong>¡Gracias por confiar en nosotros!</strong>
           </ul>
         </div>
         
