158ac36ce1112be7791d297f1f2f97d40e2f9edb
Vecy Bienes Raíces <vecybienesraices@gmail.com>
2025-06-30 18:08:19 -0500

    fix: Final corrections to routing and form links

--- src/components/WelcomeScreen.jsx
+++ src/components/WelcomeScreen.jsx
@@ -32,10 +32,10 @@ function WelcomeScreen({ onStart }) {
           <p className="flex items-start">
             <span className="mt-1">✅</span>
             <span className="ml-2">Por tu seguridad y la nuestra es clave ingresar datos reales para agilizar el permisos de ingreso.</span>
-            <ul className="list-disc list-inside pl-4 space-y-2">
+          </p>
+          <ul className="list-disc list-inside pl-4 space-y-2">
             <li><strong>¡Gracias por confiar en nosotros!</strong></li>
           </ul>
-          </p>
         </div>
         
         
